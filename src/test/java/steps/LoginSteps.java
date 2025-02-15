3e5feed1801546fca5376bfd7d8299ccf00417cf
farhadjaved23 <47040241+farhadjaved23@users.noreply.github.com>
2025-02-15 22:59:36 +0000

    integrating selenium grid"

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,39 +1,54 @@
 package steps;
 
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.LoginPage;
 
-import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.testng.Assert.assertTrue;
 
 public class LoginSteps {
     private static final Logger log = LoggerFactory.getLogger(LoginSteps.class);
     private WebDriver driver;
     private LoginPage loginPage;
-    String exptectedResult = "Jackets - Tops - Men";
+    private String expectedResult = "Jackets - Tops - Men";
+    private String nodeURL = "http://192.168.0.69:4444";  // Removed extra space
 
     @Before
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void setup() throws MalformedURLException {
+        String browser = System.getProperty("browser", "chrome");  // Get from system properties, default to Chrome
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                capabilities.setBrowserName("chrome");
+                break;
+            case "firefox":
+                capabilities.setBrowserName("firefox");
+                break;
+            case "edge":
+                capabilities.setBrowserName("MicrosoftEdge");
+                break;
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
+        driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
         loginPage = new LoginPage(driver);
     }
+
 
     @After
     public void tearDown() {
@@ -44,37 +59,43 @@ public class LoginSteps {
 
     @Given("I open the login page")
     public void iOpenTheLoginPage() {
-        driver.get("https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/");
+        driver.get("https://magento.softwaretestingboard.com/customer/account/login/");
     }
 
     @When("I enter {string} and {string}")
     public void iEnterUserCredentials(String username, String password) throws InterruptedException {
-        HashMap<String,String> userData = new HashMap<>();
-        userData.put("username",username);
-        userData.put("password",password);
-        loginPage.clickDisagreeBtn();
+        HashMap<String, String> userData = new HashMap<>();
+        userData.put("username", username);
+        userData.put("password", password);
+
+        // Safe Click: Avoid errors if button is not present
+        try {
+            loginPage.clickDisagreeBtn();
+        } catch (Exception ignored) {
+            log.info("Disagree button not found, continuing...");
+        }
+
         loginPage.enterUsername(userData.get("username"));
         loginPage.enterPassword(userData.get("password"));
         loginPage.clickLoginButton();
     }
 
-
-   @Then("I should see the {string}")
+    @Then("I should see the {string}")
     public void iShouldSeeThePOutcome(String msg) {
         String actualMessage = driver.getTitle();
-        Assert.assertEquals(msg,actualMessage);
+        Assert.assertEquals(actualMessage, msg, "Login page title mismatch!");
     }
 
     @Then("I should get the list of men menus")
-    public void iShouldGetTheListOfMenMenus() throws IOException {
+    public void iShouldGetTheListOfMenMenus() {
         loginPage.hoverMenu();
         loginPage.getMenuText();
         loginPage.compareMenuText();
         loginPage.openMenuOnNewTab();
         String actualTitle = driver.getTitle();
-//        Assert.assertEquals(exptectedResult,actualTitle,"Unable to redirect on the correct page");
-//        loginPage.getAllLinks();
+        Assert.assertEquals(actualTitle, expectedResult, "Unable to redirect to the correct page");
+
+        // Random Selection
         loginPage.SelectRandomItem();
-//        loginPage.SelectRandomColor();
     }
 }
